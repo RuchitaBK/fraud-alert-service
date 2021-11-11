2deacb0b1836d34cc216864d143ee34d37fe3f9c
RuchitaBK <kshirsagarruchita@gmail.com>
2021-11-11 23:25:52 +0530

    Initial Setup

---
+++ src/main/java/com/challenge/fraudalertservice/FraudAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.fraudalertservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudAlertServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/fraudalertservice/FraudAlertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.fraudalertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudAlertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
